80b52142a21f95f715744b42f809f3468e0dae83
Ítalo Santana <italoeugenio539@gmail.com>
2025-03-27 19:44:14 -0300

    Initializing the project structure

--- main.pl
+++ main.pl
@@ -1,11 +1,64 @@
-mochila(azul).mochila(amarela).mochila(branca).mochila(verde).mochila(vermelha).
+% Fatos
+mochila(amarela). 
+mochila(azul). 
+mochila(branca). 
+mochila(verde). 
+mochila(vermelha).
 
-nome(denis).nome(joao).nome(lenin).nome(otavio).nome(will).
+nome(denis). 
+nome(joao). 
+nome(lenin). 
+nome(otavio). 
+nome(will).
 
-mes(agosto).mes(dezembro).mes(janeiro).mes(maio).mes(setembro).
+mes(agosto). 
+mes(dezembro). 
+mes(janeiro). 
+mes(maio). 
+mes(setembro).
 
-jogo(tresoumais).jogo(cacapalavras).jogo(cubovermelho).jogo(jogodaforca).jogo(logica).
+jogo('3 ou Mais'). 
+jogo('Caça Palavras'). 
+jogo('Cubo Vermelho'). 
+jogo('Jogo da Forca'). 
+jogo('Prob. de Logica').
 
-materia(biologia).materia(geografia).materia(historia).materia(matematica).materia(portugues).
+materia(biologia). 
+materia(geografia). 
+materia(historia). 
+materia(matematica). 
+materia(portugues).
 
-suco(laranja).suco(limao).suco(maracuja).suco(uva).suco(morango).
+suco(laranja). 
+suco(limao). 
+suco(maracuja). 
+suco(morango). 
+suco(uva).
+
+% Posicoes que já sabemos:
+    % Na terceira posição está quem gosta de suco de Morango.
+    % Lenin está na quinta posição.
+    % Na primeira posição está quem gosta de suco de Limão.
+
+solucao(Meninos) :-
+    Meninos = [
+        menino(Mochila1, Nome1, Mes1, Jogo1, Materia1, limao),    
+        menino(Mochila2, Nome2, Mes2, Jogo2, Materia2, Suco2),
+        menino(Mochila3, Nome3, Mes3, Jogo3, Materia3, morango),  
+        menino(Mochila4, Nome4, Mes4, Jogo4, Materia4, Suco4),
+        menino(Mochila5, lenin, Mes5, Jogo5, Materia5, Suco5)     
+    ],
+    gosta_biologia_e_morango(Meninos),
+    imprimr_lista(Meninos).
+
+gosta_biologia_e_morango(Meninos) :-
+    member(menino(_, _, _, _, biologia, morango), Meninos).
+
+% Predicado para imprimir a lista
+imprimr_lista([]).  
+imprimr_lista([H|T]) :- 
+    writeln(H),  
+    imprimr_lista(T).
+
+% Como testar:
+% ?- solucao(Meninos).
