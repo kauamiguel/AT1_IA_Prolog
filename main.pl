d9d23d89233e37c6ef01d0f3e48221e10adcdc01
kauamiguel <kauamcm@gmail.com>
2025-03-27 10:51:05 -0300

    Add/ facts on main.pl

---
+++ main.pl
@@ -0,0 +1,11 @@
+mochila(azul).mochila(amarela).mochila(branca).mochila(verde).mochila(vermelha).
+
+nome(denis).nome(joao).nome(lenin).nome(otavio).nome(will).
+
+mes(agosto).mes(dezembro).mes(janeiro).mes(maio).mes(setembro).
+
+jogo(tresoumais).jogo(cacapalavras).jogo(cubovermelho).jogo(jogodaforca).jogo(logica).
+
+materia(biologia).materia(geografia).materia(historia).materia(matematica).materia(portugues).
+
+suco(laranja).suco(limao).suco(maracuja).suco(uva).suco(morango).
